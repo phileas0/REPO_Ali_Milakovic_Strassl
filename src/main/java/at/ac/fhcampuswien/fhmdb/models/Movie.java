8493d09b6c21ce9d78b034757d197ddd8668a0f7
Phileas <phileas.strassl@stud.fh-campus.ac.at>
2024-03-11 16:44:21 +0100

    added arraylist etc.

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,16 +1,18 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
     private String title;
     private String description;
     // TODO add more properties here
-
-    public Movie(String title, String description) {
+    private List<Genre> genres;
+    public Movie(String title, String description, List<Genre> genres) {
         this.title = title;
         this.description = description;
+        this.genres = genres;
     }
 
     public String getTitle() {
@@ -21,9 +23,12 @@ public class Movie {
         return description;
     }
 
+    public List<Genre> getGenres(){ return genres;}
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
         // TODO add some dummy data here
+
+        movies.add(new Movie ("Poor Things","Porn Movie with Emma Stone and Mark Ruffalo", Arrays.asList(Genre.DRAMA, Genre.ADVENTURE, Genre.FANTASY, Genre.ROMANCE)));
 
         return movies;
     }
--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,7 +1,58 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import at.ac.fhcampuswien.fhmdb.models.Genre;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HomeControllerTest {
+    @Test
+    void list_Of_Genres_Exists() {
+        //Given
+        Genre[] expectedGenres = {
+                Genre.ACTION,
+                Genre.ADVENTURE,
+                Genre.ANIMATION,
+                Genre.BIOGRAPHY,
+                Genre.COMEDY,
+                Genre.CRIME,
+                Genre.DRAMA,
+                Genre.DOCUMENTARY,
+                Genre.FAMILY,
+                Genre.FANTASY,
+                Genre.HISTORY,
+                Genre.HORROR,
+                Genre.MUSICAL,
+                Genre.MYSTERY,
+                Genre.ROMANCE,
+                Genre.SCIENCE_FICTION,
+                Genre.SPORT,
+                Genre.THRILLER,
+                Genre.WAR,
+                Genre.WESTERN
+        };
+
+
+        //When
+        Genre[] actualGenres = Genre.values();
+
+
+        //Then
+        for (Genre expectedGenre : expectedGenres) {
+            boolean exists = false;
+            for (Genre actualGenre : actualGenres) {
+                if (expectedGenre.equals(actualGenre)) {
+                    exists = true;
+                    break;
+                }
+            }
+            assertTrue(exists, "Das Genre " + expectedGenre + " sollte in der Genre enum existieren.");
+        }
+
+    }
+
 
 }
